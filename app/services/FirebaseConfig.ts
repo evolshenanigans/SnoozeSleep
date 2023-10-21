9a93c28e612370f1a7e490c0cc1aa444c1b7c56c
evolshenanigans <gjgutierrez91@gmail.com>
2023-10-20 21:48:52 -0700

    fixed the onboarding problem

--- app/services/FirebaseConfig.ts
+++ app/services/FirebaseConfig.ts
@@ -2,11 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { KEYS } from "../key";
-// import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,11 +16,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-// const auth = initializeAuth(FIREBASE_APP, {
-//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-// });
-// export const FIREBASE_AUTH = auth;
-export const FIREBASE_AUTH = getAuth();
+
+// Initialize Firebase Auth with AsyncStorage for persistence
+const auth = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export const FIREBASE_AUTH = auth;
 const db = getFirestore(FIREBASE_APP);
 export const FIREBASE_DB = db;
+
 console.log("Is Firestore initialized?", FIREBASE_DB ? "Yes" : "No");
